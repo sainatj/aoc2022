04b10c3ba0bf98ef575745ea1561fb3de5c5f0eb
Sainath <sainaths@MacBook-Air.local>
2022-12-02 21:48:32 -0800

    Day 3 - part-1

---
+++ day03/part-1.js
@@ -0,0 +1,34 @@
+const getImport = require('../getInput');
+
+const getMatchingEl = (firstHalf, secondHalf) => {
+  let i = 0;
+
+  while(i <= firstHalf.length) {
+    const iter = firstHalf[i];
+    if(secondHalf.indexOf(iter) > -1) {
+      // ASCII of 'a' is 97 but if we want to assign 'a' as 1, offset the ASCII value by 96 (97 - 1)
+      // ASCII of 'A' is 65 but if we want to assign 'A' as 27, offset the ASCII value by 38 (65 - 27)
+      const offset = (iter == iter.toLowerCase()) ? 96 : 38;
+      return iter.charCodeAt(0) - offset;
+    };
+
+    i++;
+  }
+}
+
+const partone = () => {
+  let count = 0;
+  const res = getImport().split(/\n/);
+
+  res.map(stream => {
+    const firstHalf = stream.substring(0, stream.length/2);
+    const secondHalf = stream.substring(stream.length/2, stream.length);
+    
+    count = count + getMatchingEl(firstHalf, secondHalf);
+  });
+
+  return count;
+}
+
+const answer = partone();
+console.log('Answer: ', answer);
